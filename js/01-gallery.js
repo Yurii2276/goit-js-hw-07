e905ebf4153d1bd3130f548fd3ce2d3650b62a59
Ursx2204 <127949449+Yurii2276@users.noreply.github.com>
2023-07-19 21:37:35 +0300

    1 task + mod window

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,43 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-const
-console.log(galleryItems);
+// import * as basicLightbox from './node_modules/basiclightbox/dist/basicLightbox.min.js';
+
+
+const refs = {
+    galleryList: document.querySelector(`.gallery`),
+    galleryLink : document.querySelector(`.gallery__link`),
+}
+
+refs.galleryList.insertAdjacentHTML(`beforeend`, createGalleryMarkup(galleryItems));
+
+refs.galleryList.addEventListener('click', handleGalleryItemClick);
+
+
+function createGalleryMarkup(array) {
+    return array.map(({ preview, description, original }) =>
+        `<li class="gallery__item"><a class="gallery__link" href="${original}">
+        <img class="gallery__image"
+        src="${preview}" 
+        alt="${description}"
+        data-source="${original}"
+        ></a></li>`).join(``);
+}
+
+function handleGalleryItemClick(event) {
+    event.preventDefault();
+
+    const target = event.target;
+
+    if (target.classList.contains('gallery__image')) {
+        const source = target.dataset.source;
+        openModal(source);
+    }
+}
+
+function openModal(source) {
+    const instance = basicLightbox.create(`<img src="${source}">`);
+    instance.show();
+}
+
+
+
