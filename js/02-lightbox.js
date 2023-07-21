ab80df3562fd77198ca178de6be2cb256ff9c13b
Ursx2204 <127949449+Yurii2276@users.noreply.github.com>
2023-07-21 13:56:03 +0300

    finish

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -7,21 +7,41 @@ const refs = {
 
 refs.galleryList.insertAdjacentHTML(`beforeend`, createGalleryMarkup(galleryItems));
 
+refs.galleryList.addEventListener('click', handleGalleryItemClick);
 
 function createGalleryMarkup(array) {
-    return array.reduce((acum, )) 
-    
-    sum = numbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
-console.log(sum); // Output: 15
-    
-    array.map(({ preview, description, original }) =>
-        `<li class="gallery__item"><a class="gallery__link" href="${original}">
-        <img class="gallery__image"
-        src="${preview}" 
-        alt="${description}"
-        data-source="${original}"
-        ></a></li>`).join(``);
+    return array.reduce((acum, { preview, original, description }) => acum + `
+    <li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`, ``)
+}
+        
+
+function handleGalleryItemClick(event) {
+    event.preventDefault();
+
+    const target = event.target;
+
+    if (target.classList.contains('gallery__image')) {
+        const source = target.dataset.source;
+        openModal(source);
+    }
 }
 
+    function openModal(source) {
+    const instance = new SimpleLightbox(`.gallery a`, {
+        captions: true, captionsData: `alt`,
+        captionPosition: 'bottom',
+        captionDelay: 250,
+    });
+    instance.on('show.simplelightbox', () => {
+        
+    });
+    instance.on('error.simplelightbox', e => {
+        console.log('Error:', e); 
+    });
 
-console.log(galleryItems);
+}   
+  
